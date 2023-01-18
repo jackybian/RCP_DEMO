054495d3f31213439a0e0226cf3de0fc41f9e3ff
jackybian <happy198308@126.com>
2023-01-18 10:38:07 +0800

    update

---
+++ com.ost.jacky.demo/src/com/ost/jacky/demo/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,22 @@
+package com.ost.jacky.demo;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.ost.jacky.demo.perspective";
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+	
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
---
+++ com.ost.jacky.demo/src/com/ost/jacky/demo/Perspective.java
@@ -0,0 +1,25 @@
+package com.ost.jacky.demo;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+import com.ost.jacky.demo.util.PluginUtil;
+
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+//		layout.setEditorAreaVisible(false);
+//		layout.setFixed(true);
+		String editorArea = layout.getEditorArea();//Returns the special identifier for the editor area in this page layout
+		layout.addView(PluginUtil.NavigatorView_ID, IPageLayout.LEFT, 0.3f, editorArea);
+		IFolderLayout leftbottom = layout.createFolder("left", IPageLayout.BOTTOM, 0.4f, PluginUtil.NavigatorView_ID);
+		leftbottom.addView(PluginUtil.SearchView_ID);
+		IFolderLayout rightbottom = layout.createFolder("rightbottom", IPageLayout.BOTTOM, 0.7f, editorArea);
+		rightbottom.addView(PluginUtil.PatientInfoView_ID);
+		rightbottom.addView(PluginUtil.ExpenseInfoView_ID);
+	}
+
+}
