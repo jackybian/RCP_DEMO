2c3adea85520229dfadee7df09e15561fbaa8ad8
jackybian <happy198308@126.com>
2023-01-20 13:05:07 +0800

    第一次正式提交

--- com.ost.jacky.demo/src/com/ost/jacky/demo/Perspective.java
+++ com.ost.jacky.demo/src/com/ost/jacky/demo/Perspective.java
@@ -11,15 +11,15 @@ public class Perspective implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-//		layout.setEditorAreaVisible(false);
-//		layout.setFixed(true);
-		String editorArea = layout.getEditorArea();//Returns the special identifier for the editor area in this page layout
-		layout.addView(PluginUtil.NavigatorView_ID, IPageLayout.LEFT, 0.3f, editorArea);
-		IFolderLayout leftbottom = layout.createFolder("left", IPageLayout.BOTTOM, 0.4f, PluginUtil.NavigatorView_ID);
-		leftbottom.addView(PluginUtil.SearchView_ID);
-		IFolderLayout rightbottom = layout.createFolder("rightbottom", IPageLayout.BOTTOM, 0.7f, editorArea);
-		rightbottom.addView(PluginUtil.PatientInfoView_ID);
-		rightbottom.addView(PluginUtil.ExpenseInfoView_ID);
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+//		String editorArea = layout.getEditorArea();//Returns the special identifier for the editor area in this page layout
+//		layout.addView(PluginUtil.NavigatorView_ID, IPageLayout.LEFT, 0.3f, editorArea);
+//		IFolderLayout leftbottom = layout.createFolder("left", IPageLayout.BOTTOM, 0.4f, PluginUtil.NavigatorView_ID);
+//		leftbottom.addView(PluginUtil.SearchView_ID);
+//		IFolderLayout rightbottom = layout.createFolder("rightbottom", IPageLayout.BOTTOM, 0.7f, editorArea);
+//		rightbottom.addView(PluginUtil.PatientInfoView_ID);
+//		rightbottom.addView(PluginUtil.ExpenseInfoView_ID);
 	}
 
 }
